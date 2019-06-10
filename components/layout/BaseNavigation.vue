314c3194c8f7a871ca944748586f0140b85452ed
matthieudou <matthieu.doultremont@gmail.com>
2019-06-10 14:27:00 +0200

    refacto and add changing color of the menu depending on the scroll

--- components/layout/BaseNavigation.vue
+++ components/layout/BaseNavigation.vue
@@ -1,105 +1,68 @@
 <template>
   <header
     class="fixed w-full top-0 flex justify-between p-8 transition z-10"
-    :class="{'bg-red-100': navbarIsScrolled}"
+    :class="navbarTextColor"
   >
-    <nuxt-link
-      :to="localePath({name: 'index'})"
-    >
-      <h1 class="flex items-center">
-        <img
-          class="h-12"
-          src="~/assets/images/logo-tlc.png"
-          alt="Logo of the louise company"
-        >
-        <span class="text-xl font-black ml-4">The Louise Company</span>
-      </h1>
+    <nuxt-link :to="localePath({name: 'index'})">
+      <img
+        class="h-12"
+        src="~/assets/images/logo-tlc.png"
+        alt="Logo of the louise company"
+      >
     </nuxt-link>
 
-    <transition name="menu">
-      <nav
-        class="fixed w-64 bg-gray-300 right-0 inset-y-0 p-8 pt-24 text-right"
-        v-show="isOpen"
-      >
-        <nuxt-link
-          class="block mt-4 text-lg uppercase"
-          :to="localePath('index')"
-          @click.native="isOpen = false"
-        >
-          Accueil
-        </nuxt-link>
-        <nuxt-link
-          class="block mt-4 text-lg uppercase"
-          :to="localePath('production')"
-          @click.native="isOpen = false"
-        >
-          Production
-        </nuxt-link>
-        <nuxt-link
-          class="block mt-4 text-lg uppercase"
-          :to="localePath('events')"
-          @click.native="isOpen = false"
-        >
-          Events
-        </nuxt-link>
-        <nuxt-link
-          class="block mt-4 text-lg uppercase"
-          :to="localePath('influencers')"
-          @click.native="isOpen = false"
-        >
-          Influencers
-        </nuxt-link>
-        <nuxt-link
-          class="block mt-4 text-lg uppercase"
-          :to="localePath('contact')"
-          @click.native="isOpen = false"
-        >
-          Contact
-        </nuxt-link>
-        <div>
-          Lang switcher
-
-          <nuxt-link
-            v-for="locale in $i18n.locales"
-            :key="locale.code"
-            :to="switchLocalePath(locale.code)"
-          >
-            {{ locale.code }}
-          </nuxt-link>
-        </div>
-      </nav>
-    </transition>
+    <nuxt-link
+      class="text-center"
+      :to="localePath({name: 'index'})"
+    >
+      <div>The Louise</div>
+      <div>Company</div>
+    </nuxt-link>
 
     <icon-menu
       @click="isOpen = !isOpen"
       :is-open="isOpen"
     />
+
+    <base-menu v-show="isOpen"/>
   </header>
 </template>
 
 <script>
   import IconMenu from '~/components/layout/IconMenu'
+  import BaseMenu from '~/components/layout/BaseMenu'
 
   export default {
     data () {
       return {
         isOpen: false,
-        navbarIsScrolled: false
+        colorSections: [],
+        navbarTextColor: 'text-white'
       }
     },
 
     methods: {
       navigationScroll () {
-        if (window.pageYOffset >= 100) {
-          this.navbarIsScrolled = true
-        } else {
-          this.navbarIsScrolled = false
+        this.colorSections.forEach(sect => {
+          if (sect.offsetTop < window.pageYOffset + 32) {
+            this.navbarTextColor = sect.getAttribute('data-section-color')
+          }
+        })
+      }
+    },
+
+    watch: {
+      $route: {
+        immediate: true,
+        handler () {
+          if (process.client) this.colorSections = document.querySelectorAll('[data-section-color]')
         }
       }
     },
 
     created () {
       if (process.client) {
+        this.navigationScroll()
         document.addEventListener('scroll', this.navigationScroll)
       }
     },
@@ -109,21 +72,13 @@
     },
 
     components: {
-      IconMenu
+      IconMenu,
+      BaseMenu
     }
   }
 </script>
 
 <style scoped>
-.menu-enter-active, .menu-leave-active {
-  transition: all .5s ease-in-out;
-}
-
-.menu-enter, .menu-leave-to {
-  opacity: 0;
-  transform: translateX(100%);
-}
-
 .fadeInRight-enter-active, .fadeInRight-leave-active {
   transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
@@ -132,5 +87,4 @@
   opacity: 0;
   transform: translateX(10px);
 }
-
 </style>

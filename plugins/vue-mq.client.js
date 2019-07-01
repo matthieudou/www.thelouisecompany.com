953e4aa6f1bad58a51fdc90ace9fd4940faeaad9
matthieudou <matthieu.doultremont@gmail.com>
2019-07-01 08:59:52 +0200

    add vueMq to the project

---
+++ plugins/vue-mq.client.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueMq from 'vue-mq'
+
+Vue.use(VueMq, {
+  breakpoints: { // default breakpoints - customize this
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280
+  },
+  defaultBreakpoint: 'sm' // customize this for SSR
+})

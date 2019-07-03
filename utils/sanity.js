7f6d348d4bd7673cc97cd3ae824cc5819e88c2d9
matthieudou <matthieu.doultremont@gmail.com>
2019-07-03 09:00:32 +0200

    re-change dataset for production

--- utils/sanity.js
+++ utils/sanity.js
@@ -3,28 +3,7 @@ require('dotenv').config()
 
 export const client = sanityClient({
   projectId: process.env.SANITY_PROJECT_ID,
-  dataset: 'development' || process.env.NODE_ENV,
+  dataset: process.env.NODE_ENV || 'production',
   token: '',
   useCdn: process.env.NODE_ENV === 'production'
 })
-
-// const generateRoutesFromSlug = (string, slugObjects) => {
-//   return slugObjects.map(item => `${string}${item.slug}`)
-// }
-
-// const xQuery = `
-// *[_type == 'movie' && releaseYear >= 1979]
-// `
-
-// export const generatedRoutes = () => {
-//   const promises = [
-//     client.fetch(xQuery)
-//   ]
-
-//   return Promise.all(promises)
-//     .then(([xResponse]) => {
-//       return [
-//         ...generateRoutesFromSlug('/x/', xResponse)
-//       ]
-//     })
-// }
